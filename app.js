8c8ceb2b1b42eea6212b1d85128ecc6a01d32348
fannyhasbi <fnyhsbi@gmail.com>
2018-11-03 22:26:35 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+// Instance
+new Vue({
+  el: '#app',
+  data: {
+    kodepos: [],
+    search: '',
+    awal: 0,
+    akhir: 10,
+    halaman: 1,
+    tampil: true
+  },
+  created(){
+    this.getData();
+  },
+  methods: {
+    next(){
+      this.awal   = this.awal += 10;
+      this.akhir  = this.akhir += 10;
+      this.halaman= this.halaman += 1;
+    },
+    prev(){
+      this.awal   = this.awal -= 10;
+      this.akhir  = this.akhir -= 10;
+      this.halaman= this.halaman -= 1
+    },
+    getData(){
+      var kd = this;
+      axios.get('https://kodepos-2d475.firebaseio.com/kota_kab/k106.json?print=pretty').then(function(response){
+        kd.kodepos = response.data;
+      });
+    }
+  },
+  computed: {
+    cari(){
+      return this.kodepos.filter(pos => {
+        return pos.kecamatan.toLowerCase().match(this.search.toLowerCase());
+      });
+    }
+  }
+});
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js').then(function(){
+    console.log('SW Registered');
+  });
+}
